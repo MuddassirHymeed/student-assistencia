3d49a8b9d9cdb7d9cf61e5cf78c091235e51ca21
MuddassirHymeed <mhk.csit@gmail.com>
2024-06-20 12:40:26 +0500

    modify mongodb URL

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongodbUrl: 'mongodb+srv://abdul_wasay:wasey@048f2019105048@studentassistencia-l2idn.mongodb.net/test?retryWrites=true&w=majority'
-}
+    mongodbUrl: "mongodb://localhost:27017/student-assistencia",
+};
